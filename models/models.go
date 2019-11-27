7170bd45cf966bac3af8b2195668bbff661c8022
Hejiadong <820873255@qq.com>
2019-11-27 21:28:39 +0800

    0
    test

--- models/models.go
+++ models/models.go
@@ -42,7 +42,7 @@ func Lookup(user string) (int64, MysqlUser , error){
 
 // 下面这个函数用于修改对应user的check,key表示的是加密前的东西
 // 验证过程：收到前端发过来的MD5，后端自身根据原有check字段产生新的MD5，进行对比
-// 相同则验证成功，然后新的MD5赋值给check，并且返回
+// 相同则验证成功，然后新的MD5赋值给check，并且返回，
 func Change_check(user string, key string) error {
 	_, err := orm.NewOrm().QueryTable("userinfo").Filter("Name", user).Update(orm.Params{
 		"Check": key,
